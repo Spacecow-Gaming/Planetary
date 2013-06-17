51b4f0175be282df0686e7937f3cf4dc30390739
SumDuud <sumduud63@gmail.com>
2013-06-17 11:26:30 +0100

    Added map gen, prettified output

--- Python/planetary.py
+++ Python/planetary.py
@@ -1,14 +1,18 @@
 #!/bin/env python
 # Copyright (c) 2013 Spacecow Gaming
+"Planetary, the game. Needs no more explanation"
 import sys
 
-def mainMenu():
-    "Prints the main menu"
+def mainmenu():
+    "Simple main menu function"
     print ("Planetary: A game of sorts")
     print ("(1) Start Game\n(2) Continue Game\n(3) Exit")
     action = int(input("Selection: "))
     if action == 1:
-        print ("\n\n\nYour journey into the cosmos is beginning. You think you are high.\nIs any of this real? The only way to find out is to engage your inner demons in space combat!\n\n\n")
+        print ("\n\n\nYour journey into the cosmos is beginning.\n"
+                "You think you are high. Is any of this real?\n"
+                "The only way to find out is to engage your inner\n" 
+                "demons in space combat!\n\n\n")
         start()
     elif action == 2:
         print ("There is no save game function")
@@ -16,36 +20,81 @@ def mainMenu():
         sys.exit()
     else:
         print ("That is not an option, try again")
-    mainMenu()
+    mainmenu()
 
 class Sector:
     "Base unit of board" 
-    description = "Nothing here."
-    # Other things will be added here
+    description = ""
+    objects = "Some hydrogen atoms"
+    def __init__(self, indesc):
+        self.description = indesc
+    def getdesc(self):
+        "Returns description"
+        return self.description
+    def getobj(self):
+        "Will return a list of things in sector, for now, a string"
+        return self.objects
+    def setobj(self, newobj):
+        "Will modify list of things in sector, for now, takes a string"
+        self.objects = newobj
+    # Other things will be added here e.g. planets, other players
 
 class Player:
     "The player's spaceship"
-    position = (0,0)
-    dead = False
+    position = (0, 0)
+    def __init__(self, inpos):
+        self.position = inpos
+    def getpos(self):
+        "Returns player position tuple"
+        return self.position
+    def setpos(self, newpos):
+        "Sets player position, takes a tuple"
+        self.position = newpos
 
-def generateBoard():
+def generateboard():
+    "Returns a dictionary of Sectors with their coordinates as the key"
     board = {}
-    for x in range(11):
-        for y in range(11):
-            board[(x, y)] = Sector()
+    for xpos in range(11):
+        for ypos in range(11):
+            board[(xpos, ypos)] = Sector("There are stars here, surprisingly")
     return board
 
+def generatemap(starsys):
+    "Will turn the starsystem dictionary into a nice string"
+    mapstr = ""
+    for xpos in range(11):
+        for ypos in range(11):
+            mapstr += "*"
+        mapstr += "\n"
+    return mapstr
+
+    
+
 def start():
-    starSystem = generateBoard()
-    newPlayer = Player()
+    "Starts the game, returns to menu on return"
+    starsystem = generateboard()
+    player1 = Player((0, 0))
+    clear = "\n" * 1000
+    output = ""
+    bar = "=" * 70
     while True:
-        print ("You are in sector:", newPlayer.position )
-        print ("Sector description:", starSystem[newPlayer.position].description)
-        action = input ("Enter action:")
+        print (clear)
+        print (generatemap(starsystem))
+        print (output)
+        print (bar)
+        print ("INFO: You are in sector:\t", 
+                player1.position )
+        print ("INFO: Sector description:\t", 
+                starsystem[player1.position].getdesc())
+        print ("INFO: Things in sector:\t\t", 
+                starsystem[player1.position].getobj())
+        action = input (bar +
+                        "\n"
+                        "Enter action:")
         if action == "help":
-            print ("\nType help for help.\n")
+            output =  "HELP: Type help for help.\n"
         else:
-            print ("\nNot a valid action.\n")
+            output = "ERROR: Not a valid action.\n"
 
 
-mainMenu()
+mainmenu()
