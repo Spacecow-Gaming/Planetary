5d0e7dd727d598f0c4e79fc7f21b5af707375b1d
SumDuud <sumduud63@gmail.com>
2013-06-20 17:24:46 +0100

    Improved the build system

--- Java/compile.bat
+++ Java/compile.bat
@@ -2,13 +2,6 @@ cd C:\Users\Will\Documents\Personal\Projects\Space Cow Gaming\Planetary\Java
 
 javac MainClass.java
 java MainClass
-del MainClass.class
-del mainMenu.class
-del game.class
-del shop.class
-del mine.class
-del sector.class
-del build.class
-del explore.class
+del *.class
 
 pause
