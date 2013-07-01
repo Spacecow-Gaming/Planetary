9b5db87c972d197ae2f95700316adb51e18a941f
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2013-07-01 23:53:17 +0100

    Fuck it, sub maps almost work. Good enough for now

--- Python/game.py
+++ Python/game.py
@@ -2,6 +2,7 @@
 "Abstract game objects which don't really affect the player"
 
 import matter
+from useful import weightedchoice
 
 class Sector:
     "Base unit of board" 
@@ -11,8 +12,7 @@ class Sector:
     def __init__(self, indesc, inobj):
         self.description = indesc
         self.objects = inobj
-    # This is shown every tick to the user - make it concise, 
-    # no more than 1 line
+
     def getdesc(self):
         "Returns description string"
         return self.description
@@ -33,8 +33,6 @@ class Sector:
             objstr += obj.getshortdesc() 
             if formatted:
                 objstr += "\n\t\t\t "
-        # Since I'm new to Python, there might be a more pythonic
-        # way to do str manipulation which I don't know about
         tmplist = list(objstr)
         tmplist = tmplist[:-5]
         objstr = "".join(tmplist)
@@ -49,22 +47,31 @@ class Sector:
         "Adds object to list"
         self.objects.append(newobj)
 
+class Supersector(Sector):
+    "Like a sector and a stripped down Board had a baby"
+    submap = {}
+    objprobs = {matter.Shop():1, matter.Gas():120}
+    def __init__(self, indesc, inobj):
+        self.description = indesc
+        self.objects = inobj
+        for xpos in range(11):
+            for ypos in range(11):
+                newsect = Sector("Nothing", [])
+                newsect.addobj(weightedchoice(self.objprobs))
+                self.submap[(xpos, ypos)] = newsect
+
+    def getmap(self):
+        return self.submap
+
 class Board:
     "Essentially a container for a data structure of sectors"
 
     # As of now, the structure de jour is a dictionary
     starsys = {}
     
-    # Generic objects
-    planet, asteroid = matter.Planet(), matter.Asteroid()
-    nothing = matter.Gas()
-
-    # Special, rarer ones
-    pcolony, acolony = matter.PColony(), matter.AColony()
-
     # List of objects and their relative probabilities of occurring
-    objprobs = { planet:10, asteroid:30, nothing:300,
-                 pcolony:5, acolony:15 }
+    objprobs = { matter.Planet():10, matter.Asteroid():30, matter.Gas():300,
+                 matter.PColony():5, matter.AColony():15 }
 
     def __init__(self):
         for xpos in range(11):
@@ -73,8 +80,8 @@ class Board:
                 # Iterates through dictionary, using the weighted choice
                 # function to pick an object to assign as a value, which 
                 # is a list of strings, for the key, which is a tuple
-                newsect = Sector("Nothing of note", [])
-                newsect.addobj(matter.weightedchoice(self.objprobs))
+                newsect = Supersector("Nothing of note", [])
+                newsect.addobj(weightedchoice(self.objprobs))
                 self.starsys[(xpos, ypos)] = newsect
 
     def getstarsys(self):
@@ -85,14 +92,14 @@ class Board:
         "Returns sector object"
         return self.starsys[coords]
 
-def generatemap(starsys):
-    "Turns the starsystem dictionary into a nice string"
+def generatemap(tomap, size):
+    "Turns a starsystem dictionary into a nice string"
     mapstr = ""
-    for xpos in range(11):
-        for ypos in range(11):
+    for xpos in range(size):
+        for ypos in range(size):
             # Iterates through all sectors in the starsystem, picking an
             # appropriate character to put into the map string
-            cursect = starsys[(xpos, ypos)]
+            cursect = tomap[(xpos, ypos)]
             curobjdescs = [""]
 
             # Translates shortdesc to map symbol
@@ -108,9 +115,9 @@ def generatemap(starsys):
                 mapstr += "a"
             elif "Asteroid Colony" in curobjdescs:
                 mapstr += "c"
+            elif "Shop" in curobjdescs:
+                mapstr += "$"
             else:
                 mapstr += "-"
         mapstr += "\n"
     return mapstr
-
-
