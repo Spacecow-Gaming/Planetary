0015b58b164354de3bd43f5e6b8b6496bf214566
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2013-07-03 21:32:45 +0100

    Switched to SFML

--- Prototype3/Game.cs
+++ Prototype3/Game.cs
@@ -1,9 +1,59 @@
 ﻿using System;
+using SFML.Window;
+using SFML.Graphics;
 
 namespace Planetary
 {
     /// <summary>
-    /// Board of sectors, from which map is drawn
+    /// This is actually a singleton
+    /// </summary>
+    class PlanetaryGame
+    {
+        /// <summary>
+        /// The entry point of the program
+        /// </summary>
+        static void Main(string[] args)
+        {
+            PlanetaryGame Game = new PlanetaryGame();
+        }
+
+        /// <summary>
+        /// Ctor which starts the main menu
+        /// </summary>
+        public PlanetaryGame()
+        {
+            MainMenu();
+        }
+
+        /// <summary>
+        /// Draws main menu window, associated buttons
+        /// </summary>
+        private void MainMenu()
+        {
+            Window MenuWindow = 
+                new Window(new VideoMode(640, 480), "Planetary");
+            MenuWindow.SetVisible(true);
+            MenuWindow.Closed += 
+                delegate(Object o, EventArgs e) { MenuWindow.Close(); };
+
+            while (MenuWindow.IsOpen())
+            {
+                MenuWindow.DispatchEvents();
+                MenuWindow.Display();
+            }
+        }
+
+        /// <summary>
+        /// Draws game, associated UI components
+        /// </summary>
+        private void StartGame()
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// Contains some sectors and other data
     /// </summary>
     class Board
     {
@@ -11,7 +61,7 @@ namespace Planetary
     }
 
     /// <summary>
-    /// Sub unit of Board, contains things in sector
+    /// Contains lists of objects, data about sector
     /// </summary>
     class Sector
     {
