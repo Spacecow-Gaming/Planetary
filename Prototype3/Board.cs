6d362d0f41edf528046160e521bd6f4c3f9dff2e
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2013-07-10 22:50:54 +0100

    Fixed static var in player class

--- Prototype3/Board.cs
+++ Prototype3/Board.cs
@@ -2,23 +2,29 @@
 using SFML.Graphics;
 
 /// <summary>
-/// Contains some sectors and other data
-/// </summary>
-class Board
-{
-    public Sprite SBoard { get; set; }
-    public Board(string ImgPath)
-    {
-        Texture TBoard = new Texture(ImgPath);
-        SBoard = new Sprite(TBoard);
-    }
-
-}
-
-/// <summary>
 /// Contains lists of objects, data about sector
 /// </summary>
 class Sector
 {
 
 }
+
+/// <summary>
+/// Contains some sectors and other data
+/// </summary>
+class Board
+{
+    public Sprite SBoard { get; set; }
+
+    /// <summary>
+    /// Initiates board sprite, texture, sectors
+    /// </summary>
+    /// <param name="ImgPath"></param>
+    public Board(string ImgPath)
+    {
+        Texture TBoard = new Texture(ImgPath);
+        SBoard = new Sprite(TBoard);
+    }
+}
+
+
