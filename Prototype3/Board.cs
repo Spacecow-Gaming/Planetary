85b6718ee6ce4c34151a0ed4644d774addb71b28
SumDuud <sumduud63@gmail.com>
2013-07-10 22:36:24 +0100

    Board implementation

---
+++ Prototype3/Board.cs
@@ -0,0 +1,24 @@
+﻿using SFML.Window;
+using SFML.Graphics;
+
+/// <summary>
+/// Contains some sectors and other data
+/// </summary>
+class Board
+{
+    public Sprite SBoard { get; set; }
+    public Board(string ImgPath)
+    {
+        Texture TBoard = new Texture(ImgPath);
+        SBoard = new Sprite(TBoard);
+    }
+
+}
+
+/// <summary>
+/// Contains lists of objects, data about sector
+/// </summary>
+class Sector
+{
+
+}
