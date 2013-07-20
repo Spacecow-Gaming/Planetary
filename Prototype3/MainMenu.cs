3a5acb6e1613ca47a263933fd87a57c86c7b4c0b
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2013-07-20 13:46:57 +0100

    Cleaned up image rendering

--- Prototype3/MainMenu.cs
+++ Prototype3/MainMenu.cs
@@ -13,61 +13,54 @@ class MainMenu
         MainMenu menu = new MainMenu();
     }
 
+    private RenderWindow window;
+
     public MainMenu()
     {
-        RenderWindow WMenu =
+        window =
             new RenderWindow(new VideoMode(640, 480), "Planetary");
-        WMenu.SetVisible(true);
+        window.SetVisible(true);
 
-        // These are the buttons
-        Texture TExit = new Texture("Media/exit.png");
-        Sprite SExit = new Sprite(TExit);
-        Texture TStart = new Texture("Media/start.png");
-        Sprite SStart = new Sprite(TStart);
+        // These are the nice-looking things
+        Sprite Title = new Sprite(new Texture("Media/title.png"));
+        Sprite Background = new Sprite(new Texture("Media/menuback.png"));
 
-        // These are the nice-looking things which aren't strictly
-        // necessary
-        Texture TTitle = new Texture("Media/title.png");
-        Sprite STitle = new Sprite(TTitle);
-        Texture TBackground = new Texture("Media/menuback.png");
-        Sprite SBackground = new Sprite(TBackground);
-
-        // This is the actual button i.e. handles the clicks
-        Button BExit = new Button(SExit, WMenu);
-        Button BStart = new Button(SStart, WMenu);
+        // These are buttons
+        AdvImage BExit = new AdvImage("Media/exit.png", window);
+        AdvImage BStart = new AdvImage("Media/Start.png", window);
 
         // Loads up the game and its files etc, will be started soon
         Game MainGame = new Game();
 
         // Guess what happens when you close the window? It CLOSES!
-        WMenu.Closed +=
-            delegate(Object o, EventArgs e) { WMenu.Close(); };
+        window.Closed +=
+            delegate(Object o, EventArgs e) { window.Close(); };
 
         BExit.Click +=
-            delegate(Object o, EventArgs e) { WMenu.Close(); };
+            delegate(Object o, EventArgs e) { window.Close(); };
 
         BStart.Click +=
-            delegate(Object o, EventArgs e) { MainGame.Start(WMenu); };
+            delegate(Object o, EventArgs e) { MainGame.Start(window); };
 
 
-        while (WMenu.IsOpen())
+        while (window.IsOpen())
         {
             // Handles input according to handlers set above
-            WMenu.DispatchEvents();
+            window.DispatchEvents();
 
             // Magic numbers? YES. Fuck the police.
-            SBackground.Position = new Vector2f(0, 0);
-            STitle.Position = new Vector2f(224, 0);
-            SStart.Position = new Vector2f(288, 200);
-            SExit.Position = new Vector2f(288, 240);
+            Background.Position = new Vector2f(0, 0);
+            Title.Position = new Vector2f(224, 0);
+            BStart.sprite.Position = new Vector2f(288, 200);
+            BExit.sprite.Position = new Vector2f(288, 240);
 
             // Renders everything
-            WMenu.Clear(Color.Magenta);
-            WMenu.Draw(SBackground);
-            WMenu.Draw(STitle);
-            WMenu.Draw(SStart);
-            WMenu.Draw(SExit);
-            WMenu.Display();
+            window.Clear(Color.Magenta);
+            window.Draw(Background);
+            window.Draw(Title);
+            window.Draw(BExit.sprite);
+            window.Draw(BStart.sprite);
+            window.Display();
         }
 
     }
